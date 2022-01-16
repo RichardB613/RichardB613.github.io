753872a2d1fb2e0ecefee05474da5dc9a39c2eca
RichardB613 <93774190+RichardB613@users.noreply.github.com>
2022-01-16 11:08:41 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -26,22 +26,3 @@ $('#navbar a, .btn').on('click', function(event) {
 
 
 
-document.querySelector('#send-mail').addEventListener('click', () => {
-  let name = document.querySelector('#name')
-  let email = document.querySelector('#email')
-  let phone = document.querySelector('#phone')
-  let message = document.querySelector('#message')
-
-  if (name.value != "" && email.value != "" && phone.value != "" && message.value != "") {
-    let body = `
-      Name: ${name.value} 
-      Email: ${email.value}
-      Phone: ${phone.value}
-      Message: "${message.value} " 
-    `
-    window.location = `mailto:info@oakclinicalsolutions.com?body=${body}`;
-    name.value ="", email.value="", phone.value="", message.value=""
-  } else {
-    alert("All fields must be filled")
-  }
-})
